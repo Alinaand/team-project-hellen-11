97ed6a1a949a1e354a49691b1ce767dfa69b3d86
Roman Panakhno <roman.panakhno.nd@gmail.com>
2021-07-09 16:23:14 +0300

    feature/header_mobile_menu
    +
    +
    +

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 (()=>{
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const logoMenuRef = document.querySelector("[menu-logo-hidden]");
 
     menuBtnRef .addEventListener("click", ()=> {
         const expanded =
@@ -10,7 +11,7 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
-
+        logoMenuRef.classList.toggle("logo-hidden");
     });
 
 })();
